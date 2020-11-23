09e5466caf94207e53ae0310418ec1da4740e6ab
walt(월트) <jaehun.lee@solarconnect.kr>
2020-11-23 16:20:29 +0900

    네임스페이스 변경, .net standard 2.0지원 기능 추가

--- src/Npgg.MemberAssigner/MemberAssigner.cs
+++ src/Npgg.MemberAssigner/MemberAssigner.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 
-namespace Npgg
+namespace Npgg.Reflection
 {
 
     public class MemberAssigner
--- src/Npgg.MemberAssigner/MemberAssignerPool.cs
+++ src/Npgg.MemberAssigner/MemberAssignerPool.cs
@@ -4,7 +4,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace Npgg
+namespace Npgg.Reflection
 {
     public class MemberAssignerPool
     {
